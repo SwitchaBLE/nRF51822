7689d4d38cf01a4458534a1c0f1a5c0578234cf7
brettbyer <brettbyer@gmail.com>
2013-11-01 13:24:34 -0500

    First Commit

---
+++ switchable_ble_app/ble_switchable.c
@@ -0,0 +1,250 @@
+/* Copyright (c) 2012 Nordic Semiconductor. All Rights Reserved.
+ *
+ * The information contained herein is property of Nordic Semiconductor ASA.
+ * Terms and conditions of usage are described in detail in NORDIC
+ * SEMICONDUCTOR STANDARD SOFTWARE LICENSE AGREEMENT.
+ *
+ * Licensees are granted free, non-transferable use of the information. NO
+ * WARRANTY of ANY KIND is provided. This heading must NOT be removed from
+ * the file.
+ *
+ */
+
+#include "ble_switchable.h"
+#include <string.h>
+#include "nordic_common.h"
+#include "ble_srv_common.h"
+#include "app_util.h"
+
+
+/**@brief Connect event handler.
+ *
+ * @param[in]   p_switchable       LEDButton Service structure.
+ * @param[in]   p_ble_evt   Event received from the BLE stack.
+ */
+static void on_connect(ble_switchable_t * p_switchable, ble_evt_t * p_ble_evt)
+{
+    p_switchable->conn_handle = p_ble_evt->evt.gap_evt.conn_handle;
+}
+
+
+/**@brief Disconnect event handler.
+ *
+ * @param[in]   p_switchable       LEDButton Service structure.
+ * @param[in]   p_ble_evt   Event received from the BLE stack.
+ */
+static void on_disconnect(ble_switchable_t * p_switchable, ble_evt_t * p_ble_evt)
+{
+    UNUSED_PARAMETER(p_ble_evt);
+    p_switchable->conn_handle = BLE_CONN_HANDLE_INVALID;
+}
+
+
+/**@brief Write event handler.
+ *
+ * @param[in]   p_switchable       LEDButton Service structure.
+ * @param[in]   p_ble_evt   Event received from the BLE stack.
+ */
+static void on_write(ble_switchable_t * p_switchable, ble_evt_t * p_ble_evt)
+{
+    ble_gatts_evt_write_t * p_evt_write = &p_ble_evt->evt.gatts_evt.params.write;
+    
+   if ((p_evt_write->handle == p_switchable->led_char_handles.value_handle) &&
+       (p_evt_write->len == 1) &&
+       (p_switchable->led_write_handler != NULL))
+   {
+       p_switchable->led_write_handler(p_switchable, p_evt_write->data[0]);
+   }
+}
+
+
+void ble_switchable_on_ble_evt(ble_switchable_t * p_switchable, ble_evt_t * p_ble_evt)
+{
+    switch (p_ble_evt->header.evt_id)
+    {
+        case BLE_GAP_EVT_CONNECTED:
+            on_connect(p_switchable, p_ble_evt);
+            break;
+            
+        case BLE_GAP_EVT_DISCONNECTED:
+            on_disconnect(p_switchable, p_ble_evt);
+            break;
+            
+        case BLE_GATTS_EVT_WRITE:
+            on_write(p_switchable, p_ble_evt);
+            break;
+            
+        default:
+            break;
+    }
+}
+
+
+/**@brief Add LED state characteristic.
+ *
+ * @param[in]   p_switchable        LEDButton Service structure.
+ * @param[in]   p_switchable_init   Information needed to initialize the service.
+ *
+ * @return      NRF_SUCCESS on success, otherwise an error code.
+ */
+static uint32_t led_char_add(ble_switchable_t * p_switchable, const ble_switchable_init_t * p_switchable_init)
+{
+    ble_gatts_char_md_t char_md;
+    ble_gatts_attr_t    attr_char_value;
+    ble_uuid_t          ble_uuid;
+    ble_gatts_attr_md_t attr_md;
+    
+
+    memset(&char_md, 0, sizeof(char_md));
+    
+    char_md.char_props.read   = 1;
+    char_md.char_props.write  = 1;
+    char_md.p_char_user_desc  = NULL;
+    char_md.p_char_pf         = NULL;
+    char_md.p_user_desc_md    = NULL;
+    char_md.p_cccd_md         = NULL;
+    char_md.p_sccd_md         = NULL;
+    
+    ble_uuid.type = p_switchable->uuid_type;
+    ble_uuid.uuid = SWITCHABLE_UUID_LED_CHAR;
+    
+    memset(&attr_md, 0, sizeof(attr_md));
+
+    BLE_GAP_CONN_SEC_MODE_SET_OPEN(&attr_md.read_perm);
+    BLE_GAP_CONN_SEC_MODE_SET_OPEN(&attr_md.write_perm);
+    
+    attr_md.vloc       = BLE_GATTS_VLOC_STACK;
+    attr_md.rd_auth    = 0;
+    attr_md.wr_auth    = 0;
+    attr_md.vlen       = 0;
+    
+    memset(&attr_char_value, 0, sizeof(attr_char_value));
+
+    attr_char_value.p_uuid       = &ble_uuid;
+    attr_char_value.p_attr_md    = &attr_md;
+    attr_char_value.init_len     = sizeof(uint8_t);
+    attr_char_value.init_offs    = 0;
+    attr_char_value.max_len      = sizeof(uint8_t);
+    attr_char_value.p_value      = NULL;
+    
+    return sd_ble_gatts_characteristic_add(p_switchable->service_handle, &char_md,
+                                               &attr_char_value,
+                                               &p_switchable->led_char_handles);
+}
+
+/**@brief Add Button state characteristic.
+ *
+ * @param[in]   p_switchable        LEDButton Service structure.
+ * @param[in]   p_switchable_init   Information needed to initialize the service.
+ *
+ * @return      NRF_SUCCESS on success, otherwise an error code.
+ */
+static uint32_t button_char_add(ble_switchable_t * p_switchable, const ble_switchable_init_t * p_switchable_init)
+{
+    ble_gatts_char_md_t char_md;
+    ble_gatts_attr_md_t cccd_md;
+    ble_gatts_attr_t    attr_char_value;
+    ble_uuid_t          ble_uuid;
+    ble_gatts_attr_md_t attr_md;
+    
+
+    memset(&cccd_md, 0, sizeof(cccd_md));
+
+    BLE_GAP_CONN_SEC_MODE_SET_OPEN(&cccd_md.read_perm);
+    BLE_GAP_CONN_SEC_MODE_SET_OPEN(&cccd_md.write_perm);
+    
+    cccd_md.vloc = BLE_GATTS_VLOC_STACK;
+    
+    memset(&char_md, 0, sizeof(char_md));
+    
+    char_md.char_props.read   = 1;
+    char_md.char_props.notify = 1;
+    char_md.p_char_user_desc  = NULL;
+    char_md.p_char_pf         = NULL;
+    char_md.p_user_desc_md    = NULL;
+    char_md.p_cccd_md         = &cccd_md;
+    char_md.p_sccd_md         = NULL;
+    
+    ble_uuid.type = p_switchable->uuid_type;
+    ble_uuid.uuid = SWITCHABLE_UUID_BUTTON_CHAR;
+
+    memset(&attr_md, 0, sizeof(attr_md));
+
+    BLE_GAP_CONN_SEC_MODE_SET_OPEN(&attr_md.read_perm);
+    BLE_GAP_CONN_SEC_MODE_SET_OPEN(&attr_md.write_perm);
+    
+    attr_md.vloc       = BLE_GATTS_VLOC_STACK;
+    attr_md.rd_auth    = 0;
+    attr_md.wr_auth    = 0;
+    attr_md.vlen       = 0;
+    
+    memset(&attr_char_value, 0, sizeof(attr_char_value));
+
+    attr_char_value.p_uuid       = &ble_uuid;
+    attr_char_value.p_attr_md    = &attr_md;
+    attr_char_value.init_len     = sizeof(uint8_t);
+    attr_char_value.init_offs    = 0;
+    attr_char_value.max_len      = sizeof(uint8_t);
+    attr_char_value.p_value      = NULL;
+    
+    return sd_ble_gatts_characteristic_add(p_switchable->service_handle, &char_md,
+                                               &attr_char_value,
+                                               &p_switchable->button_char_handles);
+}
+
+
+uint32_t ble_switchable_init(ble_switchable_t * p_switchable, const ble_switchable_init_t * p_switchable_init)
+{
+    uint32_t   err_code;
+    ble_uuid_t ble_uuid;
+
+    // Initialize service structure
+    p_switchable->conn_handle       = BLE_CONN_HANDLE_INVALID;
+    p_switchable->led_write_handler = p_switchable_init->led_write_handler;
+    
+    // Add base UUID to softdevice's internal list. 
+    ble_uuid128_t base_uuid = SWITCHABLE_UUID_BASE;
+    err_code = sd_ble_uuid_vs_add(&base_uuid, &p_switchable->uuid_type);
+    if (err_code != NRF_SUCCESS)
+    {
+        return err_code;
+    }
+    
+    ble_uuid.type = p_switchable->uuid_type;
+    ble_uuid.uuid = SWITCHABLE_UUID_SERVICE;
+
+    err_code = sd_ble_gatts_service_add(BLE_GATTS_SRVC_TYPE_PRIMARY, &ble_uuid, &p_switchable->service_handle);
+    if (err_code != NRF_SUCCESS)
+    {
+        return err_code;
+    }
+    
+    err_code = button_char_add(p_switchable, p_switchable_init);
+    if (err_code != NRF_SUCCESS)
+    {
+        return err_code;
+    }
+    
+    err_code  = led_char_add(p_switchable, p_switchable_init);
+    if (err_code != NRF_SUCCESS)
+    {
+        return err_code;
+    }
+    
+    return NRF_SUCCESS;
+}
+
+uint32_t ble_switchable_on_button_change(ble_switchable_t * p_switchable, uint8_t button_state)
+{
+    ble_gatts_hvx_params_t params;
+    uint16_t len = sizeof(button_state);
+    
+    memset(&params, 0, sizeof(params));
+    params.type = BLE_GATT_HVX_NOTIFICATION;
+    params.handle = p_switchable->button_char_handles.value_handle;
+    params.p_data = &button_state;
+    params.p_len = &len;
+    
+    return sd_ble_gatts_hvx(p_switchable->conn_handle, &params);    
+}
+
---
+++ switchable_ble_app/ble_switchable.h
@@ -0,0 +1,104 @@
+/* Copyright (c) 2012 Nordic Semiconductor. All Rights Reserved.
+ *
+ * The information contained herein is property of Nordic Semiconductor ASA.
+ * Terms and conditions of usage are described in detail in NORDIC
+ * SEMICONDUCTOR STANDARD SOFTWARE LICENSE AGREEMENT.
+ *
+ * Licensees are granted free, non-transferable use of the information. NO
+ * WARRANTY of ANY KIND is provided. This heading must NOT be removed from
+ * the file.
+ *
+ */
+
+/** @file
+ *
+ * @defgroup ble_sdk_srv_switchable SwitchaBLE Service
+ * @{
+ * @ingroup ble_sdk_srv
+ * @brief SwitchaBLE Service module.
+ *
+ * @details This module implements the SwitchaBLE Service with the Battery Level characteristic.
+ *          During initialization it adds the SwitchaBLE Service and Battery Level characteristic
+ *          to the BLE stack dataswitchablee. Optionally it can also add a Report Reference descriptor
+ *          to the Battery Level characteristic (used when including the SwitchaBLE Service in
+ *          the HID service).
+ *
+ *          If specified, the module will support notification of the Battery Level characteristic
+ *          through the ble_switchable_battery_level_update() function.
+ *          If an event handler is supplied by the application, the SwitchaBLE Service will
+ *          generate SwitchaBLE Service events to the application.
+ *
+ * @note The application must propagate BLE stack events to the SwitchaBLE Service module by calling
+ *       ble_switchable_on_ble_evt() from the from the @ref ble_stack_handler callback.
+ */
+
+#ifndef BLE_switchable_H__
+#define BLE_switchable_H__
+
+#include <stdint.h>
+#include <stdbool.h>
+#include "ble.h"
+#include "ble_srv_common.h"
+
+#define SWITCHABLE_UUID_BASE {0x26, 0xC1, 0xE1, 0x5C, 0xFB, 0x09, 0x37, 0x0D, 0x9D, 0x92, 0x47, 0x1B, 0x00, 0x00, 0x00, 0x00}
+//#define SWITCHABLE_UUID_BASE {0x23, 0xD1, 0xBC, 0xEA, 0x5F, 0x78, 0x23, 0x15, 0xDE, 0xEF, 0x12, 0x12, 0x00, 0x00, 0x00, 0x00}
+#define SWITCHABLE_UUID_SERVICE 0x6D59
+#define SWITCHABLE_UUID_LED_CHAR 0x6D5A 
+#define SWITCHABLE_UUID_BUTTON_CHAR 0x6D5B
+
+// Forward declaration of the ble_switchable_t type. 
+typedef struct ble_switchable_s ble_switchable_t;
+
+/**@brief SwitchaBLE Service event handler type. */
+typedef void (*ble_switchable_led_write_handler_t) (ble_switchable_t * p_switchable, uint8_t new_state);
+
+/**@brief SwitchaBLE Service init structure. This contains all options and data needed for
+ *        initialization of the service.*/
+typedef struct
+{
+    ble_switchable_led_write_handler_t   led_write_handler;                   
+} ble_switchable_init_t;
+
+
+/**@brief SwitchaBLE Service structure. This contains various status information for the service. */
+typedef struct ble_switchable_s
+{         
+    uint16_t                     service_handle;                 
+    ble_gatts_char_handles_t     led_char_handles;          
+    ble_gatts_char_handles_t     button_char_handles;          
+    uint8_t                      uuid_type;
+    uint8_t                      current_led_state;             
+    uint16_t                     conn_handle;  
+    bool                         is_notifying;
+    ble_switchable_led_write_handler_t  led_write_handler;
+} ble_switchable_t;
+
+
+/**@brief Initialize the SwitchaBLE Service.
+ *
+ * @param[out]  p_switchable       
+ * @param[in]   p_switchable_init  
+ *
+ * @return      NRF_SUCCESS on successful initialization of service, otherwise an error code.
+ */
+uint32_t ble_switchable_init(ble_switchable_t * p_switchable, const ble_switchable_init_t * p_switchable_init);
+
+/**@brief SwitchaBLE Service BLE stack event handler.
+ *
+ * @details Handles all events from the BLE stack of interest to the SwitchaBLE Service.
+ *
+ * @param[in]   p_switchable      SwitchaBLE Service structure.
+ * @param[in]   p_ble_evt  Event received from the BLE stack.
+ */
+void ble_switchable_on_ble_evt(ble_switchable_t * p_switchable, ble_evt_t * p_ble_evt);
+
+/**@brief Handler to notify the SwitchaBLE service on button presses.
+ *
+ * @param[in]   p_switchable      SwitchaBLE Service structure.
+ * @param[in]   p_ble_evt  Event received from the BLE stack.
+ */
+uint32_t ble_switchable_on_button_change(ble_switchable_t * p_switchable, uint8_t button_state);
+
+#endif // BLE_switchable_H__
+
+/** @} */
